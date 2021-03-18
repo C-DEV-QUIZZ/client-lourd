56421be594d1e4800a080f0c211d8afc26833aad
fabrice ferrere <fabrice.ferrere@ipilyon.net>
2021-03-18 20:59:27 +0100

    recuperation de la vue pour la creationn ds boutons personnalisés

--- Mesi Software/ViewModel/modeSoloViewModel.cs
+++ Mesi Software/ViewModel/modeSoloViewModel.cs
@@ -1,7 +1,11 @@
 ﻿using Mesi_Software.Utils;
+using Mesi_Software.View;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace Mesi_Software.ViewModel
 {
@@ -9,6 +13,12 @@ namespace Mesi_Software.ViewModel
     {
         private string _questionEnCours { get; set; } = Constantes.FAKE_QUESTION;
         public string questionEnCours { get => _questionEnCours +" ?"; set { _questionEnCours = value; OnPropertyChanged(); } }
+
+        public RelayCommand LoadedCommand => new RelayCommand(exeFunction => onLoad());
+
+
+        MainWindowsViewModel windowsViewModel;
+        private modeSolo _view;
 
         public modeSoloViewModel() 
         {
@@ -20,7 +30,18 @@ namespace Mesi_Software.ViewModel
 
         }
 
+        public void onLoad()
+        {
+            // recuperation de la page:
+            windowsViewModel = Application.Current.MainWindow.DataContext as MainWindowsViewModel;
+             _view =  windowsViewModel.CurrentPage as modeSolo;
 
+
+            // création des boutons associé a la réponse
+            Button bouton = new Button();
+            bouton.Content = "zizi";
+            _view.grid_reponses.Children.Add(bouton);
+        }
         // Creer une méthode qui récupère la réponse et l'id de la question et on stocke dans une collection
         // clé / valeur.
         // on supprime le premiere élément de la liste et on actualise _questionsEnCours.
