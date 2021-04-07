ef6f3f330b77133aa2979e1d0fa20d19fbaadca3
fabrice ferrere <fabrice.ferrere@ipilyon.net>
2021-04-07 12:06:26 +0200

    - Ajout des textes

--- Mesi Software/Utils/Constantes.cs
+++ Mesi Software/Utils/Constantes.cs
@@ -6,12 +6,11 @@ namespace Mesi_Software.Utils
 {
     public static class Constantes
     {
-        public const string FAKE_QUESTION = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eleifend arcu eu purus mollis commodo. Phasellus aliquet mi risus, venenatis blandit neque mattis eget. Donec enim orci, vestibulum non ullamcorper vel, tincidunt vitae velit. Etiam aenean.";
+        public const string QUESTION_DEFAUT = "Aucune question n'est actuellement disponible, veuillez contacter les developpeurs";
 
         public const string KEY_CHEMIN_BACK_END = "chemin";
 
         public const string MODE_SOLO_VALUE_BACK_END = "mode-solo";
-
 
         public class Routes
         {
@@ -26,5 +25,42 @@ namespace Mesi_Software.Utils
         {
             public const string ERROR_MESSAGE_NB_REPONSES_INCORRECT = "Le nombre de réponse de la question est inférieur à 2";
         }
+
+    }
+
+    public static class ConstantesTexteApplication
+    {
+        public const string TITRE_SCORE = "VOTRE SCORE";
+
+        public const string SCORE_PSEUDO = "PSEUDO";
+
+        public const string SCORE_POINTS = "POINTS";
+
+        public const string TITRE_PSEUDO = "VOTRE PSEUDO";
+
+        public const string ACCUEIL_SOLO = "JOUER SOLO";
+
+        public const string ACCUEIL_MULTI = "JOUER MULTI";
+
+        public const string ACCUEIL_QUITTER = "QUITTER";
+
+        public const string BTN_VALIDER = "VALIDER";
+
+        public const string BTN_HOME = "🏠";
+
+        public const string TOOL_TIPS_BTN_MODE_SOLO = "Cliquez pour démarrer une partie en mode solo";
+
+        public const string TOOL_TIPS_BTN_MODE_MULTI = "Cliquez pour démarrer une partie en mode multijoueur";
+
+        public const string TOOL_TIPS_BTN_QUITTER = "Cliquez pour fermer l'application";
+
+        public const string TOOL_TIPS_BTN_MODE_NOT_IMPLEMENT = "Le mode de jeu n'est pas encore implémenter";
+
+        public const string TOOL_TIPS_BTN_HOME = "Cliquez pour revenir sur la page d'accueil";
+
+        public const string TOOL_TIPS_BTN_VALIDE_PSEUDO = "Cliquez pour valider votre pseudo";
+
+        public const int BORDER_RADIUS_BOUTON_MATERIAL = 5;
+
     }
 }
