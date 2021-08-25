38d32dd19aae08f6067655ff90dd0218dcf98f4d
Fabrice FERRERE <fabrice.ferrere@orencash.fr>
2021-08-25 14:43:15 +0200

    integration websocket sharp

--- Mesi Software/ViewModel/PseudoViewModel.cs
+++ Mesi Software/ViewModel/PseudoViewModel.cs
@@ -1,16 +1,14 @@
-﻿using Mesi_Software.Utils;
-using static Mesi_Software.Utils.Enums;
+﻿using Mesi_Software.Tools;
+using Mesi_Software.Utils;
+using Mesi_Software.View;
 using System;
 using System.Collections.Generic;
-using System.Text;
-using Mesi_Software.View;
-using System.Windows;
-using Mesi_Software.Tools;
 using System.Diagnostics;
-using System.Net.Http;
 using System.Text.Json;
+using System.Windows;
 using System.Windows.Controls;
-using System.Threading.Tasks;
+using WebSocketSharp;
+using static Mesi_Software.Utils.Enums;
 
 namespace Mesi_Software.ViewModel
 {
@@ -64,6 +62,10 @@ namespace Mesi_Software.ViewModel
                 case modeJeu.solo:
                     page=sendValue(modeJeu.solo);
                     break;
+                case modeJeu.multi:
+                    testWebSocket();
+                    page = sendValue(modeJeu.multi);
+                    break;
                 default:
                     throw new Exception(Constantes.Message.ERROR_MESSAGE_MODE_NOT_IMPLEMENT);
             }
@@ -86,6 +88,19 @@ namespace Mesi_Software.ViewModel
             var dictionnaireResultChemin = JsonSerializer.Deserialize<Dictionary<string, string>>(result);
 
             return dictionnaireResultChemin[Constantes.KEY_CHEMIN_BACK_END] == Constantes.MODE_SOLO_VALUE_BACK_END ? new modeSolo() : throw new Exception("Non implémenter");
+        }
+
+        private void testWebSocket()
+        {
+            WebSocket ws = new WebSocket("ws://localhost:3000");
+
+            ws.Connect();
+
+            
+
+
+
+
         }
     }
 }
